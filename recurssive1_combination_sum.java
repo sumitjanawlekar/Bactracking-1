fc29224f8f7661216f6a8cf4dd63cbbdc59b17e4
Sumit Janawlekar <janawlekar.s@husky.neu.edu>
2020-11-22 16:18:26 -0500

    recurrsive solution for combination_sum problem

---
+++ recurssive1_combination_sum.java
@@ -0,0 +1,39 @@
+// Time Complexity : O(n^m), where n is the length of the candidates array and m in the target
+// Space Complexity : O(n+m), where n is the length of the candidates array and m in the target
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    List<List<Integer>> result;
+    int targ;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        result = new ArrayList<>();
+        targ= target;
+        helper(candidates, new ArrayList<>(), 0, 0);
+        return result;
+    }
+    
+    private void helper(int[] candidates, List<Integer> temp, int sum, int index){
+        //base
+        //sad path
+        if(sum>targ || index == candidates.length) return;
+        //happy path
+        if(sum == targ){
+            result.add(temp);
+            return;
+        }
+        //logic
+        
+        //not choose
+        //create a new copy of the temp (because though we are not choosing at every level we are creating a new reference of temp)
+        helper(candidates, new ArrayList<>(temp), sum, index+1);
+            
+        //choose 
+        //add the current candidate to the temp and create a new copy of temp
+        temp.add(candidates[index]);
+        helper(candidates, new ArrayList<>(temp), sum+candidates[index], index); 
+    }
+}
